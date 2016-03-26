96d5c8b0c22843937d112be9fdf50a9a3d73cd21
Russ Ripley <russellripley@hotmail.com>
2016-03-26 11:32:34 -0700

    wire up homepage

--- public/application/controllers/Pages.php
+++ public/application/controllers/Pages.php
@@ -2,7 +2,10 @@
 class Pages extends CI_Controller {
 
 		public function home() {
+			$this->load->model('thread');
+			$data['threads'] = $this->thread->get_all();
 			$this->load->view('templates/header');
+			$this->load->view('pages/home', $data);
 			$this->load->view('templates/footer');
 		}
 		public function register() {
