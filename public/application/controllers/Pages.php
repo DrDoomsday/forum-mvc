1991974700c7032b5facfd450fced18366b6d786
Russ Ripley <russellripley@hotmail.com>
2016-05-28 11:20:08 -0700

    fix controller.php file

--- public/application/controllers/Pages.php
+++ public/application/controllers/Pages.php
@@ -14,40 +14,4 @@ class Pages extends CI_Controller {
 			$this->load->view('helpers/database_helper');
 			$this->load->view('pages/account');
 		}
-		public function delete_post() {
-			$this->load->view('helpers/database_helper');
-			$this->load->view('pages/delete_post');
-		}
-		public function delete_thread() {
-			$this->load->view('helpers/database_helper');
-			$this->load->view('pages/delete_thread');
-		}
-		public function init_form() {
-			$this->load->view('helpers/database_helper');
-			$this->load->view('pages/init_form');
-		}
-		public function make_thread() {
-			$this->load->view('templates/header');
-			$this->load->view('templates/footer');
-			$this->load->view('pages/init_form');
-			$this->load->view('pages/make_thread');
-		}
-		public function new_password() {
-			$this->load->view('helpers/database_helper');
-			$this->load->view('pages/new_password');
-		}
-		public function register() {
-			$this->load->view('pages/init_form');
-			$this->load->view('pages/register');
-		}
-		public function reply() {
-			$this->load->view('helpers/database_helper');
-			$this->load->view('pages/reply');
-		}
-		public function view_thread() {
-			$this->load->view('helpers/database_helper');
-			$this->load->view('templates/header');
-			$this->load->view('templates/footer');
-			$this->load->view('pages/view_thread');
-		}
 }
