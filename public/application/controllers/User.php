d73029220ae0f5bf8a448beba887c6abe319a91d
d00msday <russellripley@hotmail.com>
2016-06-01 21:47:16 -0700

    fix User controller

--- public/application/controllers/User.php
+++ public/application/controllers/User.php
@@ -1,28 +1,63 @@
-<?php
-
-class User extends CI_Controller {
-	public function login() {
-		$this->load->model('user_model');
-
-		$results = $this->user_model->login(
-			$_POST['username'],
-			$_POST['password']
-		);
-
-		if (count($results) > 0) {
-			$_SESSION['user'] = $results[0]->username;
-			$this->load->helper('url');
-			redirect('http://rdripley.com/forum-mvc');
-		} else {
-			$this->load->view('pages/login_failed');
-		}
-	}
-
-	public function logout() {
-		unset($_SESSION['user']);
-		unset($_SESSION['user_id']);
-
-		$this->load->helper('url');
-		redirect('http://rdripley.com/forum-mvc');
-	}
-}
+<?php
+
+
+
+class User extends CI_Controller {
+
+	public function login() {
+
+		$this->load->model('user_model');
+
+
+
+		$results = $this->user_model->login(
+
+			$_POST['username'],
+
+			$_POST['password']
+
+		);
+
+
+
+		if (count($results) > 0) {
+
+			$_SESSION['user'] = $results[0]->username;
+
+			$this->load->helper('url');
+
+			redirect('http://rdripley.com/forum-mvc');
+
+		} else {
+
+			$this->load->view('pages/login_failed');
+
+		}
+
+	}
+
+
+
+	public function logout() {
+
+		unset($_SESSION['user']);
+
+		unset($_SESSION['user_id']);
+
+
+
+		$this->load->helper('url');
+
+		redirect('http://rdripley.com/forum-mvc');
+
+	}
+
+	public function register() {
+		$this->load->view('common/header');
+		$this->load->view('pages/register');
+		$this->load->view('common/footer');
+
+	}
+
+}
+
